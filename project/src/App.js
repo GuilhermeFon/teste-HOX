220a1eb38dece8296c824b2b368835ef78a5721a
Guilherme Fonseca <guilhermelimafonseca@gmail.com>
2022-07-27 19:11:31 -0300

    att

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Logo from './assets/Logo.png'
 import './App.css';
 
 function App() {
   return (
     <>
       <header className="App-header">
-        
+        <section className="Container_Nav">
+          <img src={Logo} className="Logo"/>
+          <div className="Container_Menu">
+            <p>HOME</p>
+            <p>TEAM</p>
+            <p>TOPICS</p>
+            <p>CONTACT</p>
+          </div>
+        </section>
+        <section className='Container_Card'>
+          
+        </section>
       </header>
     </>
   );
