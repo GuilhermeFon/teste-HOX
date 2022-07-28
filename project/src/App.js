3a47db36215d3cf8255f91ee7dedc52effebb9b7
Guilherme Fonseca <guilhermelimafonseca@gmail.com>
2022-07-28 20:35:53 -0300

    att

--- project/src/App.js
+++ project/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Logo from './assets/Logo.png'
+import Arrow from './assets/Arrow.png'
+import FotoOne from './assets/People01.png'
+import FotoTwo from './assets/People02.png'
+import FotoThree from './assets/People03.png'
 import './App.css';
 
 function App() {
@@ -7,7 +11,7 @@ function App() {
     <>
       <header className="App-header">
         <section className="Container_Nav">
-          <img src={Logo} className="Logo"/>
+          <img src={Logo} className="Logo" />
           <div className="Container_Menu">
             <p>HOME</p>
             <p>TEAM</p>
@@ -16,11 +20,40 @@ function App() {
           </div>
         </section>
         <section className='Container_Card'>
-          
+          <p className='Title'>CHECK THE TOP 10 INTERIORS OF THE WEEK.</p>
+          <p className='Txt'>our team of architects and designers has put together a selection of the best interiors of the week.</p>
+          <div className='Container_Arrow'>
+            <img src={Arrow} className="Arrow" />
+          </div>
         </section>
       </header>
+      <body>
+        <h1 className='Title_Team'>MEET OUR TEAM</h1>
+        <section className='Team'>
+          <div className='People01'>
+            <img src={FotoOne} />
+            <p></p>
+            <p></p>
+            <button>See Profile</button>
+          </div>
+          <div className='People02'>
+            <img src={FotoTwo} />
+            <p></p>
+            <p></p>
+            <button>See Profile</button>
+          </div>
+          <div className='People03'>
+            <img src={FotoThree} />
+            <p></p>
+            <p></p>
+            <button>See Profile</button>
+          </div>
+        </section>
+      </body>
     </>
   );
 }
 
 export default App;
+
+
